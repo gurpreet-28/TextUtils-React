86e638cf01aee96b51f667c5facedb687ab3fb24
Jimmy <cynghjimmy0227@gmail.com>
2023-01-08 23:42:05 +0530

    added components and routers

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [alert, setAlert] = useState(null);
@@ -20,12 +21,25 @@ function App() {
 
   return (
     <>
-      <Navbar title="TextUtils" />
-      <Alert alert={alert} />
-      <div className="container mt-3">
-        <TextForm heading="Enter the text to analyze" showAlert={showAlert} />
-        {/* <About /> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" />
+        <Alert alert={alert} />
+        <div className="container mt-3">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <TextForm
+                  heading="Enter your text here"
+                  showAlert={showAlert}
+                />
+              }
+            />
+            <Route exact path="about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to="/">
           {props.title}
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -22,19 +23,19 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <Link className="nav-link active" aria-current="page" to="/">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <Link className="nav-link active" aria-current="page" to="/about">
                 About
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <Link className="nav-link active" aria-current="page" to="/">
                 Contact
-              </a>
+              </Link>
             </li>
           </ul>
           <form className="d-flex" role="search">
